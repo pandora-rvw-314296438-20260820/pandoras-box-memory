f58749f6c0c4783bd04c887837ddcba5a7da7208
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-09-01 06:53:48 +0000

    recovery(memory): restore applied migration 20260824175551

---
+++ supabase/migrations/20260824175551_revoke_direct_execute_from_projectos_evidence_review_trigger.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.ensure_projectos_evidence_review_atomic() from public, anon, authenticated;
+grant execute on function public.ensure_projectos_evidence_review_atomic() to service_role;
