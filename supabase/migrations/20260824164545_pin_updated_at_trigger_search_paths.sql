7d2e96dfeaf5a28b513ef245db2e2b72c3936a54
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-09-01 06:58:23 +0000

    recovery(memory): restore applied post-Vault migration

---
+++ supabase/migrations/20260824164545_pin_updated_at_trigger_search_paths.sql
@@ -0,0 +1,4 @@
+alter function public.set_pandora_promotion_executions_updated_at() set search_path = pg_catalog;
+alter function public.set_pandora_promotion_requests_updated_at() set search_path = pg_catalog;
+alter function public.set_pandora_shadow_context_pack_updated_at() set search_path = pg_catalog;
+alter function public.set_updated_at() set search_path = pg_catalog;
